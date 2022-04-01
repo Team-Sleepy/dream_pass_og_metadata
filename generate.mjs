19a1fcede91f130f2c348d9a0ccf57797acc856f
Lu Yu <yu.lu@shopee.com>
2022-04-01 14:59:06 +0800

    update

--- generate.mjs
+++ generate.mjs
@@ -14,7 +14,7 @@ Array.from(Array(50).keys()).forEach((i) => {
 
     name: `Dream Pass OG #${tokenId}`,
     description:
-      'Dream Pass OG is the first collection by SleepyheadzZz. Holders will have access to powerful NFT market bots on Discord, such as real-time price tracking, automated floor and token alerts, trend graphs, wallet monitoring, mint progress tracking and more!\nNo more switching between browser tabs and apps while engaging with your communities on Discord. Make your NFT journey easier with Dream Pass!',
+      'Dream Pass OG is the first collection by SleepyheadzZz. Holders will have access to powerful NFT market bots on Discord, such as real-time price tracking, automated floor and token alerts, trend graphs, wallet monitoring, mint progress tracking and more!\n\nNo more switching between browser tabs and apps while engaging with your communities on Discord. Make your NFT journey easier with Dream Pass!',
   };
   fs.writeFileSync(`./${tokenId}.json`, JSON.stringify(json, null, 2));
   // console.log(json);
